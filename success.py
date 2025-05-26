1e04bda7f7f3a3f037784d293ba2c8654a7f1d6a
RaJharit77 <hei.rajo.2@gmail.com>
2025-05-26 18:56:40 +0300

    feat: Add new files

---
+++ success.py
@@ -0,0 +1,9 @@
+def success(result):
+    if result.lower() == "success":
+        print("Congratulations!")
+    else:
+        print("It's a lesson to be learned and an opportunity" \
+        " to rise again and keep fighting without giving up.")
+
+success("success")
+success("échec")
